a4b2abfa40adaae513189e231cac8f3307ba5f58
jbep <joshuabemartin@gmail.com>
2022-07-13 11:28:39 -0400

    added files

---
+++ pokemonstyle/project one tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="project one tileset" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="First Asset pack.png" width="384" height="384"/>
+</tileset>
